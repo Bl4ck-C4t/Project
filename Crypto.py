754b78b29cc5a52c2f059dabf5da8b7cc0f12164
TheShadow88 <stef4oben88@gmail.com>
2016-01-12 19:07:23 +0200

    A Text file encrypotor

---
+++ Crypto.py
@@ -0,0 +1,31 @@
+import glob
+def asci(txt):
+    enter = txt
+    aa = ""
+    c = 0
+    for x in enter:
+        try:
+            aa += chr(ord(str(x)) - c)
+        except ValueError:
+            aa += x
+        c += 1
+    return aa
+
+for x in glob.glob("*.txt"):
+    f = open(x, "r+")
+    file = f.read()
+    f = open(x, "w")
+    f.truncate()
+    f.write(asci(file))
+    f.close()
+
+for x in glob.glob("asd.py"):
+    f = open(x)
+    file = f.read()
+    f = open(x, "w")
+    f.truncate()
+    f.write(asci(file))
+    f.close()
+
+
+print("File decrypted.")

c4c83aa05d2cd086b0cc597d332d9dc8e2c0a002
TheShadow88 <stef4oben88@gmail.com>
2016-01-12 19:10:08 +0200

    Update Crypto.py

--- Crypto.py
+++ Crypto.py
@@ -1,5 +1,6 @@
 import glob
-def asci(txt):
+print("Instructions: Place txt files with text in them in the same folder as Crypto.py, hit enter, watch magic in files")
+input()
     enter = txt
     aa = ""
     c = 0

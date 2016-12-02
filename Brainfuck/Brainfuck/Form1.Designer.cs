4067c9870d709f925f20ac941889d20cf3468602
TheShadow88 <stef4oben88@gmail.com>
2016-12-02 17:23:12 +0200

    here

---
+++ Brainfuck/Brainfuck/Form1.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace Brainfuck
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.output_box = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.Input = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.Input_box = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(292, 37);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(168, 38);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Interprete";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // output_box
+            // 
+            this.output_box.Font = new System.Drawing.Font("Arial", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.output_box.Location = new System.Drawing.Point(410, 136);
+            this.output_box.Multiline = true;
+            this.output_box.Name = "output_box";
+            this.output_box.Size = new System.Drawing.Size(323, 230);
+            this.output_box.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(134, 116);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(115, 16);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Brainfuck Input";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(539, 116);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 16);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Output";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(97, 32);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Open file";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Input
+            // 
+            this.Input.Location = new System.Drawing.Point(292, 99);
+            this.Input.Name = "Input";
+            this.Input.Size = new System.Drawing.Size(166, 22);
+            this.Input.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(345, 79);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(65, 17);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Input box";
+            // 
+            // Input_box
+            // 
+            this.Input_box.Font = new System.Drawing.Font("Courier New", 10.15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Input_box.Location = new System.Drawing.Point(31, 136);
+            this.Input_box.Name = "Input_box";
+            this.Input_box.ShowSelectionMargin = true;
+            this.Input_box.Size = new System.Drawing.Size(337, 230);
+            this.Input_box.TabIndex = 9;
+            this.Input_box.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(766, 488);
+            this.Controls.Add(this.Input_box);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.Input);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.output_box);
+            this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Brainfuck interpreter";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox output_box;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox Input;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.RichTextBox Input_box;
+    }
+}
+
---
+++ Brainfuck/Brainfuck/Form1.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+
+namespace Brainfuck
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        OpenFileDialog ofd = new OpenFileDialog();
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+        
+        private void button2_Click(object sender, EventArgs e)
+        {
+            ofd.Filter = "BF|*.txt;*.bf";
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                StreamReader str = new StreamReader(ofd.OpenFile());
+                Input_box.Text = str.ReadToEnd();
+                string info = Input_box.Text;
+                info = info.Replace("\n", "");
+                Input_box.Text = info;
+                str.Dispose();
+                if (info.Contains(','))
+                {
+                    MessageBox.Show("This program requires input.\nEnter it in the input field");
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string info = Input_box.Text;
+            List<int> ind = new List<int>();
+            ind.Add(0);
+            int curr = 0;
+            int point = 0;
+            string str = "";
+            int str_ind = 0;
+            int loop_start = 0;
+            int loop_end = 0;
+            string asd = "";
+            char x;
+            try {
+                while (point < info.Length)
+                {
+                    x = info[point];
+                    if (x == '+')
+                    {
+                        ind[curr]++;
+                    }
+                    else if (x == '-')
+                    {
+                        ind[curr]--;
+                        if (ind[curr] < 0)
+                        {
+                            ind[curr] = 0;
+                        }
+                    }
+                    else if (x == '>')
+                    {
+                        curr++;
+                        if (curr == ind.Count)
+                        {
+                            ind.Add(0);
+                        }
+                    }
+                    else if (x == '<')
+                    {
+                        curr--;
+                        if (curr < 0)
+                        {
+                            curr = 0;
+                            ind.Insert(0, 0);
+                        }
+
+                    }
+                    else if (x == '.')
+                    {
+                        asd += (char)ind[curr];
+                    }
+                    else if (x == ',')
+                    {
+                        if (str == "")
+                        {
+                          
+                            str = Input.Text;
+
+                        }
+                        if (str_ind == str.Length)
+                        {
+                            ind[curr] = 0;
+                        }
+                        else
+                        {
+                            ind[curr] = str[str_ind];
+                            str_ind++;
+                        }
+                    }
+                    else if (x == '^')
+                    {
+                        ind[curr] = Convert.ToInt32(Input.Text);
+                    }
+                    else if (x == '[')
+                    {
+                        loop_start = point;
+                        loop_end = point + info.Substring(point).IndexOf("]");
+                        if (ind[curr] == 0)
+                        {
+                            point = loop_end;
+                        }
+                    }
+                    else if (x == ']')
+                    {
+                        if (ind[curr] != 0)
+                        {
+                            point = loop_start;
+                        }
+                    }
+                    else if (x == '*')
+                    {
+                        ind[curr] *= 2;
+                    }
+                    else if (x == '/')
+                    {
+                        asd += Convert.ToString(ind[curr]);
+                    }
+                    else if (x == '!')
+                    {
+                        ind[curr] = 0;
+                    }
+                    point++;
+                }
+                output_box.Text = asd;
+            }
+            catch
+            {
+                MessageBox.Show("There was an Error");
+            }
+
+        }
+    }
+}
